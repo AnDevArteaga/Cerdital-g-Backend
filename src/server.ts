c707a5993c7cfbb41c48b93d6604ee4921453582
AnDevArteaga <ramosandres873@gmail.com>
2025-04-15 21:24:54 -0500

    Cors

--- src/server.ts
+++ src/server.ts
@@ -33,8 +33,10 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
-
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/batch', batchRoutes);
